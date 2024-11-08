d7e6023074d54cd882a10a2f6d9b629ed6d67fa6
waniasantos <waniakellys@gmail.com>
2024-11-07 21:18:05 -0300

    init

---
+++ LayoutApp/app/src/main/java/com/example/layoutapp/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.layoutapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import android.graphics.Color
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val layout = findViewById<ConstraintLayout>(R.id.layout_principal)
+        val editNome = findViewById<EditText>(R.id.editNome)
+        val btnEnviar = findViewById<Button>(R.id.btnEnviar)
+
+
+        layout.setBackgroundColor(Color.parseColor("#F0F4F8"))
+        btnEnviar.setBackgroundColor(Color.parseColor("#2196F3"))
+
+
+        btnEnviar.setOnClickListener {
+            val nome = editNome.text.toString()
+            if (nome.isNotEmpty()) {
+                Toast.makeText(this, "Olá, $nome!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Por favor, digite seu nome.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+}
