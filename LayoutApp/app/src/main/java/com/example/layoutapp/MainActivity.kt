0b3e8ee6cc2821cfbe60c8fbce56be0d12d42db4
waniasantos <waniakellys@gmail.com>
2024-11-08 16:47:58 -0300

    três pontinhos

--- LayoutApp/app/src/main/java/com/example/layoutapp/MainActivity.kt
+++ LayoutApp/app/src/main/java/com/example/layoutapp/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.layoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import android.graphics.Color
 import androidx.constraintlayout.widget.ConstraintLayout
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,4 +38,38 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    // Infla o menu na ActionBar
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    // Lida com cliques nos itens do menu
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item1 -> {
+                // Ação para o Item 1
+                Toast.makeText(this, "Ligar / Desligar selecionado", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.item2 -> {
+                // Ação para o Item 2
+                Toast.makeText(this, "Configurações selecionado", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.item3 -> {
+                // Ação para o Item 3
+                Toast.makeText(this, "Ajuda selecionado", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.item4 -> {
+                // Ação para o Item 3
+                Toast.makeText(this, "Sobre selecionado", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
