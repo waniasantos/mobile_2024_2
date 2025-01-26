667242f57251bf1bd7526d86155a2ae7552ceae9
waniasantos <waniakellys@gmail.com>
2025-01-25 22:57:38 -0300

    msg app (apk)

--- MsgApp/app/build.gradle.kts
+++ MsgApp/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.msgapp"
-        minSdk = 35
+        minSdk = 34
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
